6780c3ed04a5873219f072c6af39e366c25ae39b
Leonardo Miranda <leonardomm.contato@gmail.com>
2022-09-23 09:38:45 -0300

    DepartmentService

--- src/module-info.java
+++ src/module-info.java
@@ -4,6 +4,8 @@ module workshop_javafx_jdbc {
 	
 	exports gui;
 	opens gui to javafx.fxml;
-	
+    opens model.entities to javafx.graphics, javafx.fxml, javafx.base;
+
+    opens model.services to javafx.graphics, javafx.fxml;
 	opens application to javafx.graphics, javafx.fxml;
 }
